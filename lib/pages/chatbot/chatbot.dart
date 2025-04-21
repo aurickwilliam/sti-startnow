bc83c9e3030105dfafbb074ae7026d43039027e0
aurickwilliam <aurickwilliam21@gmail.com>
2025-04-21 20:59:40 +0800

    DONE: Chatbot UI for now

---
+++ lib/pages/chatbot/chatbot.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sti_startnow/pages/chatbot/components/chat_bubble.dart';
+import 'package:sti_startnow/pages/components/page_app_bar.dart';
+import 'package:sti_startnow/theme/app_theme.dart';
+
+class Chatbot extends StatefulWidget {
+  const Chatbot({super.key});
+
+  @override
+  State<Chatbot> createState() => _ChatbotState();
+}
+
+class _ChatbotState extends State<Chatbot> {
+
+  final List<List> messages = [
+    ["Hello Chatbot", true],
+    ["Good afternoon, what can I help you today?", false],
+    ["Thank you", true],
+  ];
+
+  final TextEditingController messageController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.colors.white,
+
+      // Content
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              PageAppBar(
+                title: "ChatBot"
+              ),
+
+              const SizedBox(height: 20,),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    
+                    // if empty pa ung messages
+                    // for future backend
+                    if (messages.isEmpty){
+                      return Center(
+                        child: Text("Chat Now!"),
+                      );
+                    }
+
+                    // Return a chat bubble
+                    return ChatBubble(
+                      message: messages[index][0], 
+                      isUser: messages[index][1]
+                    );
+                  }
+                )
+              ),
+
+              // Chat Box and Send Button
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: messageController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        
+                        // If Focused
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: AppTheme.colors.primary, 
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+
+                        // Not Focused
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: AppTheme.colors.gray,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+
+                        hintText: "Say Hello.."
+                      ),
+
+                      // Text style
+                      style: GoogleFonts.roboto(
+                        color: AppTheme.colors.black,
+                      ),
+                    ),
+                  ),
+
+                  IconButton(
+                    onPressed: () {}, 
+                    icon: Icon(
+                      Icons.send_rounded,
+                      color: AppTheme.colors.primary,
+                      size: 30,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
