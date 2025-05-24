0579c007043561d5720fabea0abc72267be22575
aurickwilliam <aurickwilliam21@gmail.com>
2025-05-24 19:33:23 +0800

    ADDED: Custom toast for downloaded

---
+++ lib/pages/enrollment/components/download_toast.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sti_startnow/theme/app_theme.dart';
+
+class DownloadToast extends StatelessWidget {
+  const DownloadToast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppTheme.colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x40000000),
+            blurRadius: 4.0,
+            offset: Offset(0, 1),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage("assets/img/enrollment/check-mark.png"),
+              fit: BoxFit.contain,
+              width: 50,
+            ),
+        
+            const SizedBox(height: 20,),
+        
+            Text(
+              "Downloaded",
+              style: GoogleFonts.roboto(
+                color: AppTheme.colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
