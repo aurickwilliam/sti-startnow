eb40752d46fc35d095a45ccd0d1c3d6b9cea154e
Sanois <wgmsoriano@gmail.com>
2025-05-29 00:14:30 +0800

    UPDATED: chatbot kb

--- cb_backend/startnow_chatbot.py
+++ cb_backend/startnow_chatbot.py
@@ -24,7 +24,7 @@ knowledge_base = {
     ("bye", "thanks", "thank you"): "You’re welcome!  Feel free to message again for any inquiries.",
 
     #intent: Enrollment period
-    ("enrollment date", "enrollment period", "when is the enrollment"): "Enrollment is open from May 1st and ends on August 30th.",
+    ("enrollment date", "enrollment period", "when is the enrollment"): "Enrollment is open from May 1st and ends on July 31st. For irregular students: June 1st to July 31st.",
 
     #intent: Enrollment Requirement
     ("what are the requirements", "what do I need for enrollment", "requirements"): (
@@ -48,6 +48,7 @@ knowledge_base = {
 
     #intent: Enrollment Process
     ("how do i enroll", "how to resgister my enrollment", "how does enrollment work", "enrollment process", "enrollment"): (
+        "Enrollment is open from May 1st and ends on July 31st.\n"
         "Here are the steps to proceed with the enrollment using our application:\n"
         "- Go to the Main Dashboard and click on the ‘Enroll Now’ button.\n"
         "- For new students, enter your desired course.\n"
@@ -58,7 +59,8 @@ knowledge_base = {
     ),
 
     #intent: Irregular Student Enrollment Process
-    ("how does the enrollment process for irregular students work", "irregular student enrollment"): (
+    ("how does the enrollment process for irregular students work", "irregular student enrollment", "irregular enrollment"): (
+        "For irregular students, enrollment period starts on June 1st until July 31st.\n"
         "Here are the steps to proceed with the enrollment using our application:\n"
         "- Go to the Main Dashboard and click on the ‘Enroll Now’ button.\n"
         "- Make sure to have your approved subject checklist beforehand.\n"
@@ -78,7 +80,7 @@ knowledge_base = {
     ("how can i cancel my enrollment", "cancel enrollment", "withdraw application"): "Cancellation or withdrawal of enrollment requires an exit clearance from the registrar.",
 
     #intent: Available Courses
-    ("available courses", "what courses are available", "list of courses"):  (
+    ("available courses", "what courses are available", "list of courses", "courses"):  (
         "The available courses in STI Caloocan are:\n"
         "- BS Computer Science(BSCS)\n"
         "- BS Information Technology(BSIT)\n"
@@ -96,19 +98,19 @@ knowledge_base = {
     ("drop subject", "dropping of subjects", "add or drop a subject after enrollment"): "You may add or drop subjects through our app only during a specific adjustment period from September August 30th – September 10th.",
 
     #intent: Class Schedule
-    ("when do classes start", "class schedule", "first day of class"): "Classes officially starts on September 1. Your class schedule is visible on your account page after your enrollment is successful.",
+    ("when do classes start", "class schedule", "first day of class", "start of school"): "Classes officially starts on September 1. Your class schedule is visible on your account page after your enrollment is successful.",
 
     #intent: Section Picking
     ("can i pick my own section", "can i choose my own section"): "You may pick your section based on the available schedules offered.",
 
     #intent: Grade Viewing
-    ("where to view my grades", "view my grades", "how can i check my grades online"): "You may view your grades through the OneSTI portal.",
+    ("where to view my grades", "view my grades", "how can i check my grades online", "grades"): "You may view your grades through the OneSTI portal.",
 
     #intent: Tuition Fee
     ("how much is the tuition", "tuition fee", "tuition"): "Tuition fee vary by course or the number of units taken. You may visit the 'Tuition Fee Advising Page' for an estimate.",
 
     #intent: Remaining Balance
-    ("remaining balance", "standing balance"): "You may view any remaining balance through the OneSTI portal or requirest a statement of account at the Cashier’s Office.",
+    ("remaining balance", "standing balance", "balance"): "You may view any remaining balance through the OneSTI portal or requirest a statement of account at the Cashier’s Office.",
 
     #intent: Deadlines
     ("deadline of requirements", "when is the deadline of document submission?"): "Students are given ample time to gather their documents, but it’s strongly encouraged to submit them early to avoid enrollment issues in future semesters.",
@@ -117,7 +119,7 @@ knowledge_base = {
     ("how to pay tuition fee", "where can i pay", "Does the school accept online payments", "payment process"): "All payments must be made in person at the Cashier’s Office. Online payments methods such as Gcash are available.",
 
     #intent: Scholarship and Discounts
-    ("available scholarships", "available discounts", "scholarship"): "The school offers various financial assistant programs such as subsidy and voucher, government educational assistance, and open scholarships.", 
+    ("available scholarships", "available discounts", "scholarship", "scholarships", "discount"): "The school offers various financial assistant programs such as subsidy and voucher, government educational assistance, and open scholarships.", 
 
     #intent: Scholarship and Discounts Details
     ("how to apply for financial assistance", "where to submit my scholarship requirements", "application for scholarship"): "You may apply for STI’s scholarship, voucher, and financial assistance programs by coordinating directly with the campus. Application forms and requirements are provided onsite.",
