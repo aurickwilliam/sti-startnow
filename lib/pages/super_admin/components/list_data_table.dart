67826a86383cfdde13fc8162b636baaa62514e26
aurickwilliam <aurickwilliam21@gmail.com>
2025-05-29 22:16:17 +0800

    FIX: sizing of data table in super admin

--- lib/pages/super_admin/components/list_data_table.dart
+++ lib/pages/super_admin/components/list_data_table.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sti_startnow/theme/app_theme.dart';
 
 class ListDataTable extends StatelessWidget {
   final List<List> dataTableValues;
@@ -14,38 +16,45 @@ class ListDataTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scrollbar(
-      thickness: 5,
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: DataTable(
-          showCheckboxColumn: false,
-          // Specify the columns
-          columns: columnNames.map((item) {
-            return DataColumn(
-              label: Text(
-                item,
-              )
-            );
-          }).toList(),
-        
-          // Specify Each Row
-          // Automate this using map? when working on backend
-          rows: dataTableValues.map((item) {
-            return DataRow(
-              cells: item.map((value){
-                return DataCell(
-                  Text(value)
-                );
-              }).toList(),
-
-              onSelectChanged: (selected) {
-                if (selected!) {
-                  handleNavigation(item);
-                }
-              },
-            );
-          }).toList(),
+    return Container(
+      width: double.infinity,
+      child: Scrollbar(
+        thickness: 5,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            headingTextStyle: GoogleFonts.roboto(
+              color: AppTheme.colors.primary,
+              fontWeight: FontWeight.bold,
+            ),
+            showCheckboxColumn: false,
+            // Specify the columns
+            columns: columnNames.map((item) {
+              return DataColumn(
+                label: Text(
+                  item,
+                )
+              );
+            }).toList(),
+          
+            // Specify Each Row
+            // Automate this using map? when working on backend
+            rows: dataTableValues.map((item) {
+              return DataRow(
+                cells: item.map((value){
+                  return DataCell(
+                    Text(value)
+                  );
+                }).toList(),
+          
+                onSelectChanged: (selected) {
+                  if (selected!) {
+                    handleNavigation(item);
+                  }
+                },
+              );
+            }).toList(),
+          ),
         ),
       ),
     );
